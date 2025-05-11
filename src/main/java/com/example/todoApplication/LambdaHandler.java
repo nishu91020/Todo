c098a5553139f904d7c84c3168c153a459df7fd7
Nishu Rai <rainishu111@gmail.com>
2025-05-11 13:57:57 +0530

    dummy update

--- src/main/java/com/example/todoApplication/LambdaHandler.java
+++ src/main/java/com/example/todoApplication/LambdaHandler.java
@@ -13,9 +13,6 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-/**
- * Handler for requests to Lambda function.
- */
 public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
