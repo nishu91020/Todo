8f5c60a5f478e75923930a097fd7d473dd395524
Nishu Rai <rainishu111@gmail.com>
2025-05-17 23:13:42 +0530

    updated lambda handler

--- src/main/java/com/example/todoApplication/LambdaHandler.java
+++ src/main/java/com/example/todoApplication/LambdaHandler.java
@@ -1,4 +1,5 @@
 package com.example.todoApplication;
+import org.slf4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,6 +16,8 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 
 public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
+    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(LambdaHandler.class);
+
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
@@ -22,7 +25,23 @@ public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent
 
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
+        try{
+            String path = input.getPath();
+            String method = input.getHttpMethod();
+            String body = input.getBody();
+            String queryString = input.getQueryStringParameters() != null ? input.getQueryStringParameters().toString() : "";
+            String pathParameters = input.getPathParameters() != null ? input.getPathParameters().toString() : "";
 
+            if("/auth/login".equals(path))
+            {
+                logger.info("Login request received");
+
+            }
+        }
+        catch (Exception e) {
+            logger.error("Error processing request: " + e.getMessage());
+            response.withStatusCode(500).withBody("{\"error\": \"Internal Server Error\"}");
+        }
             String output = String.format("{ \"message\": \"hello world\"}");
 
             return response
