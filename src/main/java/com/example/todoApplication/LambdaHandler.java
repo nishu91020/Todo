fde199909ad1fd0b57aec9e75114dbb399764af7
Nishu Rai <rainishu111@gmail.com>
2025-05-17 22:49:01 +0530

    updated lambda handler

--- src/main/java/com/example/todoApplication/LambdaHandler.java
+++ src/main/java/com/example/todoApplication/LambdaHandler.java
@@ -22,24 +22,12 @@ public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent
 
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
-        try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+
+            String output = String.format("{ \"message\": \"hello world\"}");
 
             return response
                     .withStatusCode(200)
                     .withBody(output);
-        } catch (IOException e) {
-            return response
-                    .withBody("{}")
-                    .withStatusCode(500);
-        }
-    }
 
-    private String getPageContents(String address) throws IOException{
-        URL url = new URL(address);
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-            return br.lines().collect(Collectors.joining(System.lineSeparator()));
-        }
     }
 }
