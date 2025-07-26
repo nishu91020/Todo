807692734ba24650d776bb8cbedda1998804bd5f
Nishu Rai <rainishu111@gmail.com>
2025-07-26 22:54:31 +0530

    update for object mapper error

--- src/main/java/com/example/todoApplication/controller/AuthController.java
+++ src/main/java/com/example/todoApplication/controller/AuthController.java
@@ -17,7 +17,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.amazonaws.services.lambda.runtime.Context;
 
-
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -30,7 +29,7 @@ public class AuthController {
     private static final Logger logger = org.slf4j.LoggerFactory.getLogger(AuthController.class);
 
     public AuthController() {
-       this.authService = new AuthService();
+        this.authService = new AuthService();
     }
 
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
@@ -39,55 +38,64 @@ public class AuthController {
         String body = input.getBody();
 
         logger.info("AuthController received path: {} method: {}", path, method);
-        logger.info("is request to test path {}","/LambdaHandler/auth/test".equals(path) && "GET".equalsIgnoreCase(method));
-        
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            if ("/LambdaHandler/auth/signup".equals(path) && "POST".equalsIgnoreCase(method)) {
-                logger.info("Signing up user with body: {}", body);
+
+        ObjectMapper mapper = new ObjectMapper();
+        if ("/LambdaHandler/auth/signup".equals(path) && "POST".equalsIgnoreCase(method)) {
+            logger.info("Signing up user with body: {}", body);
+            try {
                 User user = mapper.readValue(body, User.class);
                 String result = authService.signup(user);
                 return new APIGatewayProxyResponseEvent()
                         .withStatusCode(201)
                         .withBody(result);
-            } else if ("/LambdaHandler/auth/login".equals(path) && "POST".equalsIgnoreCase(method)) {
-                logger.info("Logging in user with body: {}", body);
+            } catch (Exception e) {
+                logger.error("Error during signup: {}", e.getMessage());
+                return new APIGatewayProxyResponseEvent()
+                        .withStatusCode(500)
+                        .withBody("{\"error\":\"Internal Server Error\"}");
+            }
+
+        } else if ("/LambdaHandler/auth/login".equals(path) && "POST".equalsIgnoreCase(method)) {
+            logger.info("Logging in user with body: {}", body);
+            try {
                 User user = mapper.readValue(body, User.class);
                 String result = authService.validateUser(user.getUsername(), user.getPassword());
                 return new APIGatewayProxyResponseEvent()
                         .withStatusCode(200)
                         .withBody(result);
-            } else if ("/LambdaHandler/auth/test".equals(path) && "GET".equalsIgnoreCase(method)) {
-                logger.info("Testing integration");
+            } catch (Exception e) {
+                logger.error("Error during login: {}", e.getMessage());
                 return new APIGatewayProxyResponseEvent()
-                        .withStatusCode(200)
-                        .withBody("successful integration");
-            } else {
-                return new APIGatewayProxyResponseEvent()
-                        .withStatusCode(404)
-                        .withBody("{\"error\":\"Not Found\"}");
+                        .withStatusCode(500)
+                        .withBody("{\"error\":\"Internal Server Error\"}");
             }
-        } catch (Exception e) {
+
+        } else if ("/LambdaHandler/auth/test".equals(path) && "GET".equalsIgnoreCase(method)) {
+            logger.info("Testing integration");
             return new APIGatewayProxyResponseEvent()
-                    .withStatusCode(500)
-                    .withBody("{\"error\":\"Internal Server Error\"}");
+                    .withStatusCode(200)
+                    .withBody("successful integration");
+        } else {
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(404)
+                    .withBody("{\"error\":\"Not Found\"}");
         }
     }
 
     @PostMapping("/signup")
-    private ResponseEntity<String> signupUser(@RequestBody User user){ //signup
+    private ResponseEntity<String> signupUser(@RequestBody User user) { // signup
         System.out.println("Signing up user!!");
         return new ResponseEntity<>(authService.signup(user), HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    private ResponseEntity<String> loginUser(@RequestBody User user){
+    private ResponseEntity<String> loginUser(@RequestBody User user) {
         System.out.println("Logging in user!!");
-        return new ResponseEntity<>(authService.validateUser(user.getUsername(), user.getPassword()),HttpStatus.FOUND);
+        return new ResponseEntity<>(authService.validateUser(user.getUsername(), user.getPassword()), HttpStatus.FOUND);
     }
 
     @GetMapping("/test")
-    private ResponseEntity<String> test(){
-        return new ResponseEntity<>("successful integration",HttpStatus.FOUND);
+    private ResponseEntity<String> test() {
+        return new ResponseEntity<>("successful integration", HttpStatus.FOUND);
     }
 }
