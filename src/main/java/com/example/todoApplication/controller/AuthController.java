76fe13ba3f02082fbef77b20138f766ac379acd8
Nishu Rai <rainishu111@gmail.com>
2025-07-26 22:39:38 +0530

    update

--- src/main/java/com/example/todoApplication/controller/AuthController.java
+++ src/main/java/com/example/todoApplication/controller/AuthController.java
@@ -40,12 +40,18 @@ public class AuthController {
 
         logger.info("AuthController received path: {} method: {}", path, method);
         logger.info("is request to test path {}","/LambdaHandler/auth/test".equals(path) && "GET".equalsIgnoreCase(method));
-        
-        try {
-            HashMap<String, String> headers = new HashMap<>();
+
+        HashMap<String, String> headers = new HashMap<>();
             headers.put("Content-Type", "application/json");
             ObjectMapper mapper = new ObjectMapper();
-
+        if ("/LambdaHandler/auth/test".equals(path) && "GET".equalsIgnoreCase(method)) {
+                logger.info("Testing integration");
+                return new APIGatewayProxyResponseEvent()
+                        .withHeaders(headers)
+                        .withStatusCode(200)
+                        .withBody("successful integration");
+            }
+        try {
             if ("/LambdaHandler/auth/signup".equals(path) && "POST".equalsIgnoreCase(method)) {
                 logger.info("Signing up user with body: {}", body);
                 User user = mapper.readValue(body, User.class);
